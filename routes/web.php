115b9fccb6b585dc4e4c7fd57857c9d1c2da0d05
MarcelGF <marcelgarciafranca@gmail.com>
2020-10-07 19:07:56 -0300

    added routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'auth', 'prefix' => 'post'], function () {
+	Route::get('get_all', 'PostController@getAllPosts');
+	Route::post('create_post', 'PostController@createPost');
 });
